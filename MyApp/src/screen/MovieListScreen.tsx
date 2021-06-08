d2b77bfb11b58c04d52f9c77668a6452eaf787b1
Your Name <you@example.com>
2021-06-08 14:30:52 +0700

    fetch data from api

--- MyApp/src/screen/MovieListScreen.tsx
+++ MyApp/src/screen/MovieListScreen.tsx
@@ -1,10 +1,8 @@
 import React, {Component, useCallback, useEffect, useState} from 'react';
-import {Dimensions} from 'react-native';
+import {ActivityIndicator, View, RefreshControl} from 'react-native';
 import {FlatList} from 'react-native-gesture-handler';
-import {RecyclerListView, DataProvider, LayoutProvider} from 'recyclerlistview';
 import styled from 'styled-components/native';
 import MovieListItem from '../components/MovieListItem';
-const screen_width = Dimensions.get('window').width;
 const StyledText = styled.Text`
   font-size: 18px;
   color: white;
@@ -28,40 +26,43 @@ interface Movie {
   date: String;
 }
 const MovieListScreen: React.FC<Props> = () => {
-  const [isLoading, setLoading] = useState<boolean>(true);
+  const [loading, setLoading] = useState<boolean>(true);
   const [data, setData] = useState<any[]>([]);
-  let totalPages: number;
-  const [alldata, setAllData] = useState<any[]>([]);
+  const [totalPages, setTotalPages] = useState<number>(0);
+  const [page, setPage] = useState<number>(1);
+
   useEffect(() => {
+    setLoading(true);
     fetch(
-      'https://api.themoviedb.org/3/movie/now_playing?api_key=c1618550083ac39008a92222d9c8a6a9&language=en-US&page=1',
+      'https://api.themoviedb.org/3/movie/now_playing?api_key=c1618550083ac39008a92222d9c8a6a9&language=en-US&page=' +
+        page.toString(),
     )
       .then(response => response.json())
       .then(json => {
         setData(current => [...current, ...json.results]);
-        totalPages = json.total_pages;
-        // fetchData(totalPages);
-        console.log('all : ' + data.length);
+        setTotalPages(json.total_pages);
       })
       .catch(error => console.error(error))
-      .finally(() => setLoading(false));
-    
-  }, []);
-
-
-
-const fetchData = useCallback(async(totalPages: number) =>    {{
-    for (let i = 2; i <= totalPages; i++) {
-      await fetch(
-        'https://api.themoviedb.org/3/movie/now_playing?api_key=c1618550083ac39008a92222d9c8a6a9&language=en-US&page=' +
-          i.toString(),
-      )
-        .then(response => response.json())
-        .then(json => {
-          setData(current => [...current, ...json.results]);
-        });
+      .finally(() => {
+        setLoading(false);
+      });
+    if (data.length >= 80) {
+      data.splice(0, 60);
     }
-  }  }, []);
+  }, [page]);
+
+  const renderFooter = () => {
+    console.log(data.length);
+    if (loading === false) {
+      return null;
+    } else {
+      return (
+        <View style={{height: 30, marginVertical: 15}}>
+          <ActivityIndicator color={'#888'} size={50} />
+        </View>
+      );
+    }
+  };
 
   function formatMovie(movie: any): Movie {
     return {
@@ -76,7 +77,7 @@ const fetchData = useCallback(async(totalPages: number) =>    {{
     <StyledView>
       <FlatList
         data={data}
-        keyExtractor={item => item.id.toString()}
+        keyExtractor={(item, index) => index.toString()}
         renderItem={item => {
           let movie: any = formatMovie(item);
           return (
@@ -88,9 +89,19 @@ const fetchData = useCallback(async(totalPages: number) =>    {{
             />
           );
         }}
-        onEndReached={()=>{
-          console.warn('warn')
+        onEndReachedThreshold={0.4}
+        onEndReached={() => {
+          if (totalPages === 0) {
+            setLoading(true);
+          } else {
+            if (page <= totalPages) {
+              setPage(page + 1);
+            } else {
+              setPage(1);
+            }
+          }
         }}
+        ListFooterComponent={renderFooter}
       />
     </StyledView>
   );
